b86a955f33b3fea9fce48b6b13d1173ce1355f9b
pitichampeethong <piti_fee@hotmail.com>
2016-10-20 10:43:01 +0700

    Create SignUpActivity

--- app/src/main/java/com/openworldtravels/www/hypertension/FirstActivity.java
+++ app/src/main/java/com/openworldtravels/www/hypertension/FirstActivity.java
@@ -1,13 +1,38 @@
 package com.openworldtravels.www.hypertension;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton; //Shift Command + Enter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Signup Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FirstActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+            }// onClick
+        });
+
+    } // Main Method
+
+
+
+} // Main Class
