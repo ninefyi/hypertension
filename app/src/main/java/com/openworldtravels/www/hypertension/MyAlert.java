791f2498843259684e7c89406682e23409810a69
pitichampeethong <piti_fee@hotmail.com>
2016-10-20 11:30:39 +0700

    Check space finish

---
+++ app/src/main/java/com/openworldtravels/www/hypertension/MyAlert.java
@@ -0,0 +1,41 @@
+package com.openworldtravels.www.hypertension;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by pitichampeethong on 10/20/2016 AD.
+ */
+
+public class MyAlert {
+
+    //Explicit
+    private Context context;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, String titleString, String messageString) {
+        this.context = context;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }
+
+    public void myDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.nobita48);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }// myDialog Method
+
+
+} // Main class
--- app/src/main/java/com/openworldtravels/www/hypertension/SignUpActivity.java
+++ app/src/main/java/com/openworldtravels/www/hypertension/SignUpActivity.java
@@ -2,12 +2,53 @@ package com.openworldtravels.www.hypertension;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText, nameEditText;
+    private ImageView imageView;
+    private Button button;
+    private String userString, passwordString, nameString, imageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind widget
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From EditText
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+                nameString = nameEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("") || nameString.equals("")) {
+                    //Have space
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, "มีช่องว่าง", "Please Fill All Every Blank");
+                    myAlert.myDialog();
+                }
+
+            } // onClick
+        });
+
+    } // Main Method
+
+
+
+}// Main Class
